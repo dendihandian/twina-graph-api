4c504cef2fc96498ac74546997d9ba59c0bbc0a4
Dendi Handian <handian.dendi@gmail.com>
2020-09-28 22:16:04 +0700

    add default route for check

--- blueprints/test.py
+++ blueprints/test.py
@@ -3,6 +3,13 @@ from flask import Blueprint, jsonify, current_app
 testApi = Blueprint('test', __name__)
 
 
+@testApi.route('/', methods=['GET', 'POST'])
+def base():
+    return jsonify({
+        'message': 'Don\'t worry, the app is working. But nothing in here.'
+    })
+
+
 @testApi.route('/test')
 def test():
 
